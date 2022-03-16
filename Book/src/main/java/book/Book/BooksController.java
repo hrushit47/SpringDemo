778f9c46e695006efb5adfe5dd6670cbcb712560
Hrushikesh Thakur <hkthakur02@gmail.com>
2022-03-16 18:51:50 +0530

    version 2

--- Book/src/main/java/book/Book/BooksController.java
+++ Book/src/main/java/book/Book/BooksController.java
@@ -11,7 +11,9 @@ public class BooksController {
 	
 	@GetMapping("/books")
 	public List<Book> getAllBooks(){
-		return Arrays.asList(new Book(1l, "Mastering Spring 5.2" , "Ranga karanam "));
+		return Arrays.asList(new Book(1l, "Mastering Spring 5.1" , "Ranga karanam "),
+				new Book(2l, "Mastering Spring 5.2" , "Hrushi Thakur "),
+				new Book(3l, "Mastering Spring 5.3" , "Kunal Zurange "));
 	}
 	
 	
